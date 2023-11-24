0c3b0e4e1691a984adf3e36c5deaeeefaffcf187
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2023-11-24 18:27:07 +0600

    added task 2 solutions and organized the project folder structures

---
+++ task_2/task_2.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+
+forcast = pd.read_csv("./forcast.csv")
+
+
+# API endpoint to get temperature for a given date
+@app.route("/get_temperature", methods=["GET"])
+def get_temperature():
+    date = request.args.get("date")
+    
+    if date is None:
+        return jsonify({"error": "Date parameter is missing"}), 400
+    
+    try:
+        temperature = forcast.loc[forcast["ds"] == date, "yhat1"].values[0]
+        return jsonify({"date": date, "temperature": float(temperature)})
+    except IndexError:
+        return jsonify({"error": "Date not found in data"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
