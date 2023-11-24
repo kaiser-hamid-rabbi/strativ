fd986276837948c15eff72d2640cc23d1b2bac07
kaiser-hamid-rabbi <kaiser.hamid.rabbi@gmail.com>
2023-11-24 18:39:43 +0600

    reformatted code

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -11,10 +11,10 @@ forcast = pd.read_csv("./forcast.csv")
 @app.route("/get_temperature", methods=["GET"])
 def get_temperature():
     date = request.args.get("date")
-    
+
     if date is None:
         return jsonify({"error": "Date parameter is missing"}), 400
-    
+
     try:
         temperature = forcast.loc[forcast["ds"] == date, "yhat1"].values[0]
         return jsonify({"date": date, "temperature": float(temperature)})
@@ -23,5 +23,7 @@ def get_temperature():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+# endpoint-> http://127.0.0.1:5000/get_temperature?date=2024-11-30
 if __name__ == "__main__":
     app.run(debug=True)
